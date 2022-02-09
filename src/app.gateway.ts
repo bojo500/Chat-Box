bcea091a050c2fd96bf52b8f962180735942f377
Mohamed Khaled <flavasava1212@gmail.com>
2022-02-09 21:25:01 +0200

    create the server side & the client side

---
+++ src/app.gateway.ts
@@ -0,0 +1,38 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { Socket, Server } from 'socket.io';
+import { Logger } from "@nestjs/common";
+
+
+@WebSocketGateway()
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected:    ${client.id}`);
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): void {
+    this.wss.emit('msgToClient', text);
+    // return { event: 'msgToClient', data: text };
+  }
+
+}
